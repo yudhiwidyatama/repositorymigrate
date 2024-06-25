bbc9b538ca27596c9f7dd0cab2f560588a351f1c
yudhiwidyatama <yudhi.widyatama@gmail.com>
2024-06-25 09:57:30 +0700

    Update allrepositories.sh

--- allrepositories.sh
+++ allrepositories.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-oc get is -o go-template='{{range .items}}{{$ns := .metadata.namespace}}{{$nm := .metadata.name}}{{range .status.tags}}{{$tag := .tag}}{{ range .items}}{{$ns}}|{{$nm}}|{{$tag}}|{{.image}}{{"\n"}}{{end}}{{end}}{{end}}' --all-namespaces
+oc get is -o go-template='{{range .items}}{{$ns := .metadata.namespace}}{{$nm := .metadata.name}}{{range .status.tags}}{{$tag := .tag}}{{ range .items}}{{$ns}},{{$nm}},{{$tag}},{{.image}},{{.dockerImageReference}},{{"\n"}}{{end}}{{end}}{{end}}' --all-namespaces
